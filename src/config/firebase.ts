e02916c15b637ca75e415d5b1b6309f4116ced2d
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-06-02 08:22:14 +0200

    adding some styles to the create post form

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -19,5 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
 
 
--- src/pages/create-post/form.tsx
+++ src/pages/create-post/form.tsx
@@ -1,7 +1,7 @@
-import {useForm} from'react-hook-form';
+import {useForm} from 'react-hook-form'
 import * as yup from 'yup';
 import {yupResolver} from '@hookform/resolvers/yup';
-
+import {addDoc,collection} from 'firebase/firestore'
 interface CreateFormData {
     title: string
     description:string
@@ -19,21 +19,60 @@ const Schema = yup.object().shape({
 const  {register,handleSubmit, formState:{errors}} = useForm<CreateFormData>({
     resolver: yupResolver(Schema)
 })
+const postsRef = collection
 
 const onCreatePost = (data:CreateFormData) =>{
-          console.log(data)
+          console.log(data);
 }
 
     return  <div>
-        <form onSubmit={handleSubmit(onCreatePost)}>
-  <input type="text" placeholder='Titlle...'  {...register("title")} />
-  <p>{errors.title?.message}</p>
-  <textarea name="description" id="description" cols={30} rows={10} {...register(" description")}></textarea>    
-  <p>{errors.description?.message}</p> 
-    <input type="submit"  value="submit"/>    
-        
-        
-        </form>
+
+<form onSubmit={handleSubmit(onCreatePost)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+  <input
+    type="text"
+    placeholder="Title..."
+    {...register("title")}
+    style={{
+      width: '300px',
+      padding: '10px',
+      marginBottom: '10px',
+      border: errors.title ? '2px solid red' : '1px solid #ccc',
+      borderRadius: '4px',
+      outline: 'none',
+    }}
+  />
+  {errors.title && <p style={{ color: 'red', marginBottom: '10px' }}>{errors.title.message}</p>}
+  <textarea
+    id="description"
+    cols={30}
+    rows={10}
+    {...register("description")}
+    style={{
+      width: '300px',
+      height: '200px',
+      padding: '10px',
+      marginBottom: '10px',
+      border: errors.description ? '2px solid red' : '1px solid #ccc',
+      borderRadius: '4px',
+      outline: 'none',
+    }}
+  ></textarea>
+  {errors.description && <p style={{ color: 'red', marginBottom: '10px' }}>{errors.description.message}</p>}
+  <input
+    type="submit"
+    value="Submit"
+    style={{
+      backgroundColor: '#007bff',
+      color: '#fff',
+      padding: '10px 20px',
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer',
+      transition: 'background-color 0.3s ease',
+    }}
+  />
+</form>
+
 
          </div>
    
