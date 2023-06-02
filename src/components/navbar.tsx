d3b600faf56cdc55d3efcd69833424bab4a9056d
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-06-02 07:04:03 +0200

    adding some styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,44 +1,48 @@
-import { Link } from "react-router-dom"
-import {auth}from '../config/firebase'
-//the auth give us an access to the user's  information that have logged in
-import {useAuthState} from 'react-firebase-hooks/auth'
-import {signOut} from 'firebase/auth'
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 
+export const Navbar = () => {
+  const [user, loading, error] = useAuthState(auth);
 
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
 
-export const Navbar = () =>{
-    const [user , loading ,error] =useAuthState(auth)
+  return (
+    <nav className="navbar-container">
+      <ul className="navbar-menu">
+        <li className="navbar-item">
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/login" className="navbar-link">
+            Login
+          </Link>
+        </li>
+      </ul>
 
-      const signUserOut = async()=>{
-      await signOut(auth);
-      }
-
-
-
-
-
-
-    return(
-        <div>
-            <Link to="/" style={{padding:12}}>Home</Link>
-            <Link to="/login">Login</Link>
-
-<div>
-    {/* wihtout the help of useAuthStatenfrom the react-firebase-hooks
-   <p>{auth.currentUser?.displayName} </p> 
-   <p>Your email:{auth.currentUser?.email}</p>
-    <img src={auth.currentUser?.photoURL || ""} alt="user photo" width="100" height="100" />  */}
-         {/* USING UseAuthState  */}
-         { user &&
-        <>
-         <p>Your email:{user?.email}</p>
-    <img src={user?.photoURL || ""} alt="user photo" width="100" height="100" /> 
-    <button onClick={signUserOut}>Log Out</button>
-    </>
-}
-</div>
-
-        </div>
-    )
-}
+      <div className="user-profile">
+        {user && (
+          <>
+            <p className="user-email">Your email: {user?.email}</p>
+            <img
+              className="user-photo"
+              src={user?.photoURL || ""}
+              alt="user photo"
+              width="100"
+              height="100"
+            />
+            <button className="logout-button" onClick={signUserOut}>
+              Log Out
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
